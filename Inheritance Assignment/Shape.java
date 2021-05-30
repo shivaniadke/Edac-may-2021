02ab3fd60027ccd50bf1461576784ba7e8e9b903
SHIVANI ADKE <83281816+shivaniadke@users.noreply.github.com>
2021-05-30 18:10:44 +0530

    Update Shape.java

--- Inheritance Assignment/Shape.java
+++ Inheritance Assignment/Shape.java
@@ -1,3 +1,14 @@
+// Create a class named Shape with a function that prints "This is a
+// shape". Create another class named Polygon inheriting the Shape
+// class with the same function that prints "Polygon is a shape".
+// Create two other classes named Rectangle and Triangle having
+// the same function which prints "Rectangle is a polygon" and
+// "Triangle is a polygon" respectively. Again, make another class
+// named Square having the same function which prints "Square is a
+// rectangle".
+
+
+
 package lab;
 
 import org.w3c.dom.css.Rect;
