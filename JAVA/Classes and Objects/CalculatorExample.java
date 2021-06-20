0a109f6feee244c8fcb1de1365a99160c6cb3949
SHIVANI ADKE <83281816+shivaniadke@users.noreply.github.com>
2021-06-21 02:30:34 +0530

    Add files via upload

---
+++ JAVA/Classes and Objects/CalculatorExample.java
@@ -0,0 +1,20 @@
+package Day2;
+import java.util.*;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first value :");
+	    String no1 = sc.nextLine();
+		System.out.println("Enter the Second value :");
+		String no2 = sc.nextLine();
+		
+				
+		
+
+		
+
+	}
+
+}
